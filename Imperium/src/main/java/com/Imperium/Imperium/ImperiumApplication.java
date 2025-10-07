1d720e750b17b7ba205a9fcf8c5b0a6985992da3
underthedarkxx <roberto.diasfurtadoaraujo@gmail.com>
2025-10-07 19:54:06 -0300

    Inicialização do porjeto

---
+++ Imperium/src/main/java/com/Imperium/Imperium/ImperiumApplication.java
@@ -0,0 +1,13 @@
+package com.Imperium.Imperium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImperiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImperiumApplication.class, args);
+	}
+
+}
