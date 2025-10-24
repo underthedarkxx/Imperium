8b4b7aad90d57ba0b12500d43cd7cc1fd1d99e12
Arthur Braga <artbragaz@gmail.com>
2025-10-24 12:02:09 -0300

    adiciona tabela dinâmica, porem quebra o frontend

--- Backend/src/main/java/com/Imperium/Controllers/UsuarioController.java
+++ Backend/src/main/java/com/Imperium/Controllers/UsuarioController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,9 +47,11 @@ public class UsuarioController {
             "mensagem", "Usuário registrado com sucesso!"
         ));
     }
+    @GetMapping("/listar")
     public ResponseEntity<?> listarUsuarios() {
     var usuarios = usuarioRepository.findAll();
     return ResponseEntity.ok(usuarios);
 }
+
 }
 
